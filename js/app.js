1abaac01cbaaa6f0943854f2ed23f68347a983b6
George Zeis <george.zeis@gmail.com>
2015-07-14 16:10:57 -0400

    Added photo gallery and photo page

---
+++ js/app.js
@@ -0,0 +1,45 @@
+ $(document).ready(function () {
+  $('.fotorama').fotorama({
+    data: [
+      {img: 'img/untitled-1.jpg'},
+      {img: 'img/untitled-2.jpg'},
+      {img: 'img/untitled-3.jpg'},
+      {img: 'img/untitled-4.jpg'},
+      {img: 'img/untitled-5.jpg'},
+      {img: 'img/untitled-6.jpg'},
+      {img: 'img/untitled-7.jpg'},
+      {img: 'img/untitled-8.jpg'},
+      {img: 'img/untitled-9.jpg'},
+      {img: 'img/untitled-10.jpg'},
+      {img: 'img/untitled-11.jpg'},
+      {img: 'img/untitled-12.jpg'},
+      {img: 'img/untitled-13.jpg'},
+      {img: 'img/untitled-14.jpg'},
+      {img: 'img/untitled-15.jpg'},
+      {img: 'img/untitled-16.jpg'},
+      {img: 'img/untitled-17.jpg'},
+      {img: 'img/untitled-18.jpg'},
+      {img: 'img/untitled-19.jpg'},
+      {img: 'img/untitled-20.jpg'},
+      {img: 'img/untitled-21.jpg'},
+      {img: 'img/untitled-22.jpg'},
+      {img: 'img/untitled-23.jpg'},
+      {img: 'img/untitled-24.jpg'},
+      {img: 'img/untitled-25.jpg'},
+      {img: 'img/untitled-26.jpg'},
+      {img: 'img/untitled-27.jpg'},
+      {img: 'img/untitled-28.jpg'},
+      {img: 'img/untitled-29.jpg'},
+      {img: 'img/untitled-30.jpg'},
+      {img: 'img/untitled-31.jpg'},
+      {img: 'img/untitled-32.jpg'},
+      {img: 'img/untitled-33.jpg'},
+      {img: 'img/untitled-34.jpg'},
+
+      
+
+
+
+    ]
+  });
+});
